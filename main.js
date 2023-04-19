b8a85d78e237bd7a6497279678b115ac96d05472
João Paulo Moura Vais <123659279+jhops10@users.noreply.github.com>
2023-04-19 20:28:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let horas = document.getElementById ('horas');
+let minutos = document.getElementById ('minutos');
+let segundos = document.getElementById ('segundos');
+let data = document.querySelector('#data');
+
+
+const relogio = setInterval(function time() {
+
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr
+    minutos.textContent = min;
+    segundos.textContent = s;
+});
+
